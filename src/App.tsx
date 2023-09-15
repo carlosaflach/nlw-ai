fd731b27975bcb057245d578ee73e9ef26cd9d56
Carlos Afonso <carlos.flach15@gmail.com>
2023-09-14 22:51:00 -0300

    feat: Add model and temperature component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { FileVideo, Github, Upload } from 'lucide-react';
 import { Separator } from './components/ui/separator';
 import { Textarea } from './components/ui/textarea';
 import { Label } from './components/ui/label';
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from './components/ui/select';
 
 export function App() {
 	return (
@@ -62,13 +69,13 @@ export function App() {
 
 						<Separator />
 
-						<div className='space-y-1'>
+						<div className='space-y-2'>
 							<Label htmlFor='transcription_prompt'>
 								Prompt de transcrição
 							</Label>
 							<Textarea
 								id='transcription_prompt'
-								className='h-30 leading-relaxed'
+								className='h-50 leading-relaxed resize-none'
 								placeholder='Inclua palavras chaves mencionadas no video separadas por virgula (,)'
 							/>
 						</div>
@@ -77,6 +84,36 @@ export function App() {
 							Carregar vídeo
 							<Upload className='w-4 h-4 ml-2' />
 						</Button>
+					</form>
+
+					<Separator />
+
+					<form className='space-y-6'>
+						<div className='space-y-2'>
+							<Label>Modelo</Label>
+							<Select disabled defaultValue='gpt3.5'>
+								<SelectTrigger>
+									<SelectValue />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectItem value='gpt3.5'>GTP 3.5-turbo 16k</SelectItem>
+								</SelectContent>
+							</Select>
+							<span className='block text-xs text-muted-foreground italic '>
+								Você poderá customizar essa opção em breve
+							</span>
+						</div>
+
+						<Separator />
+
+						<div className='space-y-2'>
+							<Label>Temperatura</Label>
+
+							<span className='block text-xs text-muted-foreground italic '>
+								Valores mais altos tendem a deixar o resultado mais criativo e
+								com possíveis erros.
+							</span>
+						</div>
 					</form>
 				</aside>
 			</main>
