f22e13bb798af44f57e530367c62750183ab5e75
Carlos Afonso <carlos.flach15@gmail.com>
2023-09-19 13:55:01 -0300

    feat: creating route to upload videos

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from 'fastify';
 import { prisma } from './lib/prisma';
 import { getAllPropmtsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
 
 const app = fastify();
 
 app.register(getAllPropmtsRoute);
+app.register(uploadVideoRoute);
 
 app
 	.listen({
